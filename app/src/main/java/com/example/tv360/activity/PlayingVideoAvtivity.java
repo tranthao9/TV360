52d9e5a3ebd45cd2b1413124d1d588d8160e97f3
tranthao9 <tt088038@gmail.com>
2023-10-16 17:40:12 +0700

    s

--- app/src/main/java/com/example/tv360/activity/PlayingVideoAvtivity.java
+++ app/src/main/java/com/example/tv360/activity/PlayingVideoAvtivity.java
@@ -6,21 +6,16 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.media.MediaCodec;
-import android.media.MediaExtractor;
-import android.media.MediaFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.Surface;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -35,29 +30,25 @@ import com.example.tv360.checkvideo.VideoCodecChecker;
 import com.example.tv360.model.DataObjectUrlVideo;
 import com.example.tv360.retrofit.ApiService;
 import com.example.tv360.retrofit.HomeService;
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.analytics.AnalyticsListener;
+import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.TrackGroup;
 import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.source.hls.HlsDataSourceFactory;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelectionParameters;
 import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.android.exoplayer2.util.Util;
@@ -101,8 +92,7 @@ public class PlayingVideoAvtivity extends AppCompatActivity{
     TextView text_quality;
 
     PlayerView styledPlayerView;
-
-    private Surface surface;
+    HlsMediaSource hlsMediaSource ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -116,19 +106,24 @@ public class PlayingVideoAvtivity extends AppCompatActivity{
         String profileID = sharedPref.getString(KEY_PROFILEID,"");
         String accessToken = sharedPref.getString(KEY_ACCESSTOKEN,"");
         String m_andoid = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-
-
-
-
+        styledPlayerView = findViewById(R.id.playvideo);
+        trackSelector = new DefaultTrackSelector(PlayingVideoAvtivity.this);
+        TrackSelectionParameters newParameters = trackSelector.getParameters()
+                .buildUpon()
+                .setForceHighestSupportedBitrate(true)
+                .build();
+        trackSelector.setParameters((DefaultTrackSelector.Parameters)newParameters);
+        player = new SimpleExoPlayer.Builder(PlayingVideoAvtivity.this).setTrackSelector(trackSelector).build();
+        styledPlayerView.setPlayer(player);
         apiserver = ApiService.getlinknocontenttype(profileID,userID, m_andoid,"Bearer " + accessToken).create(HomeService.class);
         Call<DataObjectUrlVideo> data  = apiserver.getlinka(getIntent().getStringExtra("id"), getIntent().getStringExtra("type"));
         data.enqueue(new Callback<DataObjectUrlVideo>() {
             @Override
             public void onResponse(Call<DataObjectUrlVideo> call, Response<DataObjectUrlVideo> response) {
                 DataObjectUrlVideo urlVideo = response.body();
-                styledPlayerView = findViewById(R.id.playvideo);
+
                 fullscreen = styledPlayerView.findViewById(R.id.exo_fullscreen_icon);
-                TextView info = findViewById(R.id.textView);
+//                TextView info = findViewById(R.id.textView);
 //                info.setText(urlVideo.getData().);
                 progressBar = findViewById(R.id.progressBar);
 
@@ -136,8 +131,6 @@ public class PlayingVideoAvtivity extends AppCompatActivity{
                 ImageView setiing_play = styledPlayerView.findViewById(R.id.exo_settings_icon);
 
                 ImageView play = styledPlayerView.findViewById(R.id.pause_button);
-
-
                 setiing_play.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -198,135 +191,37 @@ public class PlayingVideoAvtivity extends AppCompatActivity{
                     }
                 });
 
-                trackSelector = new DefaultTrackSelector(PlayingVideoAvtivity.this);
-//                MappingTrackSelector.MappedTrackInfo mappedTrackInfo = trackSelector.getCurrentMappedTrackInfo();
-//                Log.d("mapinfor ", "" + mappedTrackInfo);
-//                if(mappedTrackInfo != null)
-//                {
-//                    DefaultTrackSelector.ParametersBuilder builder = trackSelector.buildUponParameters();
-//                    for (int rendererIndex = 0; rendererIndex < mappedTrackInfo.getRendererCount();rendererIndex ++)
-//                    {
-//                        TrackGroupArray trackGroupArray = mappedTrackInfo.getTrackGroups(rendererIndex);
-//                        if(trackGroupArray.length == 0)
-//                        {
-//                            continue;
-//                        }
-//                        List<Integer> selectedTracks = new ArrayList<>();
-//                        for (int groupindex =0;groupindex<trackGroupArray.length;groupindex++)
-//                        {
-//                           TrackGroup trackGroup = trackGroupArray.get(groupindex);
-//                           for (int trackindex = 0; trackindex<trackGroup.length;trackindex++)
-//                           {
-//                               Format format = trackGroup.getFormat(trackindex);
-//                               if(isSupportQuality(format))
-//                               {
-//                                   selectedTracks.add(trackindex);
-//                                   DefaultTrackSelector.Parameters parameters = builder.setSelectionOverride(
-//                                           rendererIndex,
-//                                           trackGroupArray,
-//                                           new DefaultTrackSelector.SelectionOverride(groupindex)
-//                                   ).build();
-//                                   trackSelector.setParameters(parameters);
-//                               }
-//                           }
-//
-//                        }
-//                    }
-//                }
-//
-//
-                TrackSelectionParameters newParameters = trackSelector.getParameters()
-                        .buildUpon()
-                        .setForceHighestSupportedBitrate(true)
-                        .build();
 
-//                TrackSelectionParameters trackSelectionParameters = new TrackSelectionParameters.Builder()
-//                        .setPreferredTextLanguage("en")
-//                                .setPreferredAudioLanguage("en")
-//                                        .build();
-//
-                trackSelector.setParameters((DefaultTrackSelector.Parameters)newParameters);
 
                 Log.d("Tag Dolby 2", " "+ VideoCodecChecker.issupportdolby());
-//                trackSelector.setParameters(trackSelector.buildUponParameters().setPreferredAudioLanguage("en").clearSelectionOverrides(C.TRACK_TYPE_VIDEO));
-//
-//                DefaultRenderersFactory renderersFactory;
-//
-//                DefaultLoadControl loadControl = new DefaultLoadControl.Builder().setBufferDurationsMs(25000, 50000, 1500, 2000).build();
-//
-//                @DefaultRenderersFactory.ExtensionRendererMode int extensionRendererMode = DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER;
-//
-//                renderersFactory = new DefaultRenderersFactory(PlayingVideoAvtivity.this) .setExtensionRendererMode(extensionRendererMode);
-//
-//
-//                player = new ExoPlayer.Builder(PlayingVideoAvtivity.this, renderersFactory)
-//                        .setTrackSelector(trackSelector)
-//                        .setLoadControl(loadControl)
-//                        .build();
-//
-//
-////                player = new ExoPlayer.Builder(PlayingVideoAvtivity.this).setTrackSelector(trackSelector).build();
-////                player = new ExoPlayer.Builder(PlayingVideoAvtivity.this).setTrackSelector(trackSelector).build();
-//                styledPlayerView.setPlayer(player);
-//                MappingTrackSelector.MappedTrackInfo mappedTrackInfo = trackSelector.getCurrentMappedTrackInfo();
-//                Log.d("mapinfor ", "" + mappedTrackInfo);
+
+
 //                MediaItem mediaItem = MediaItem.fromUri("http://cdn-vttvas.s3.cloudstorage.com.vn/video1/dv/output/stream.mpd");
-//               // MediaItem mediaItem = MediaItem.fromUri(urlVideo.getData().getUrlStreaming());
-//                player.setMediaItem(mediaItem);
-//                player.prepare();
-//                player.setPlayWhenReady(true);
-//                player.play();
-//                MediaExtractor extractor = new MediaExtractor();
-//                MediaCodec decoder = null;
-//                try {
-//                    extractor.setDataSource("http://cdn-vttvas.s3.cloudstorage.com.vn/video1/dv/output/stream.mpd");
-//                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                for (int i = 0; i<extractor.getTrackCount();i++)
-//                {
-//                    MediaFormat format = extractor.getTrackFormat(i);
-//                    String mime = format.getString(MediaFormat.KEY_MIME);
-//                    if(mime.startsWith("video/"))
-//                    {
-//                        extractor.selectTrack(i);
-//                        try {
-//                            decoder = MediaCodec.createDecoderByType(mime);
-//                            decoder.configure(format,surface,null,0);
-//                            break;
-//                        } catch (IOException e) {
-//                            throw new RuntimeException(e);
-//                        }
-//
-//                    }
-//                }
-//                if(decoder == null)
-//                {
-//                    return;
-//                }
-//                decoder.start();
+                hlsMediaSource = new HlsMediaSource.Factory(new DefaultHttpDataSourceFactory(Util.getUserAgent(PlayingVideoAvtivity.this, "exoplayer"))).createMediaSource(Uri.parse("http://cdn-vttvas.s3.cloudstorage.com.vn/video1/dv/output/stream.mpd"));
 
 
-                player = new SimpleExoPlayer.Builder(PlayingVideoAvtivity.this).setTrackSelector(trackSelector).build();
-//                player = ExoPlayerFactory.newSimpleInstance(PlayingVideoAvtivity.this,trackSelector,new DefaultLoadControl());
-//                player = ExoPlayerFactory.newSimpleInstance(PlayingVideoAvtivity.this
-//                       ,trackSelector, new DefaultLoadControl());
-                styledPlayerView.setPlayer(player);
-                HlsMediaSource hlsMediaSource =
-                        new HlsMediaSource.Factory(new DefaultHttpDataSourceFactory(Util.getUserAgent(PlayingVideoAvtivity.this, "app-name"))).createMediaSource(Uri.parse(urlVideo.getData().getUrlStreaming()));
-                player = ExoPlayerFactory.newSimpleInstance(PlayingVideoAvtivity.this);
-                ProgressiveMediaSource mediaSource = new ProgressiveMediaSource.Factory(
-                        new DefaultDataSourceFactory(PlayingVideoAvtivity.this,Util.getUserAgent(PlayingVideoAvtivity.this,""))
-                ).createMediaSource(Uri.parse(urlVideo.getData().getUrlStreaming()));
-//                ProgressiveMediaSource mediaSource = HlsDataSourceFactory(DefaultHttpDataSourceFactory("")).createMediaSource(Uri.parse(urlVideo.getData().getUrlStreaming()));
+                DefaultHttpDataSourceFactory mediaDataSourceFactory = new DefaultHttpDataSourceFactory(
+                        Util.getUserAgent(PlayingVideoAvtivity.this, "stackoverflow"));
+// do not meter bandwidth for manifest loading
+                DefaultHttpDataSourceFactory manifestDataSourceFactory = new DefaultHttpDataSourceFactory(
+                        Util.getUserAgent(PlayingVideoAvtivity.this, "stackoverflow"));
+// create the media source for DASH
+                MediaSource mediaSource = new DashMediaSource.Factory(
+                        new DefaultDashChunkSource.Factory(mediaDataSourceFactory),
+                        manifestDataSourceFactory)
+                        .createMediaSource(Uri.parse("http://cdn-vttvas.s3.cloudstorage.com.vn/video1/dv/output/stream.mpd"), null, null);
 
-                player.prepare(hlsMediaSource);
+// prepare the player
                 player.setPlayWhenReady(true);
+                player.prepare(mediaSource);
 
-                player.addListener(new Player.EventListener() {
+//                player.prepare(hlsMediaSource);
+//                player.setPlayWhenReady(true);
+
+                player.addAnalyticsListener(new AnalyticsListener() {
                     @Override
-                    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-                        Player.EventListener.super.onPlayerStateChanged(playWhenReady, playbackState);
+                    public void onPlayerStateChanged(EventTime eventTime, boolean playWhenReady, int playbackState) {
+                        AnalyticsListener.super.onPlayerStateChanged(eventTime, playWhenReady, playbackState);
                         if(playbackState == Player.STATE_READY){
                             progressBar.setVisibility(View.GONE);
                             player.setPlayWhenReady(true);
@@ -342,14 +237,14 @@ public class PlayingVideoAvtivity extends AppCompatActivity{
                     }
 
                     @Override
-                    public void onPlayerError(ExoPlaybackException error) {
-                        Player.EventListener.super.onPlayerError(error);
+                    public void onPlayerError(EventTime eventTime, ExoPlaybackException error) {
+                        AnalyticsListener.super.onPlayerError(eventTime, error);
                         Log.d("error player " , " "+error);
                     }
 
                     @Override
-                    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-                        Player.EventListener.super.onTracksChanged(trackGroups, trackSelections);
+                    public void onTracksChanged(EventTime eventTime, TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+                        AnalyticsListener.super.onTracksChanged(eventTime, trackGroups, trackSelections);
                         if(trackGroups.length > 0)
                         {
                             TrackGroup trackGroup = trackGroups.get(0);
@@ -363,10 +258,9 @@ public class PlayingVideoAvtivity extends AppCompatActivity{
                                     Log.d("Tag Dolby codec ", " "+ codec);
                                 }
                             }
-                        }
+                        };
                     }
                 });
-
             }
             @Override
             public void onFailure(Call<DataObjectUrlVideo> call, Throwable t) {
@@ -375,19 +269,6 @@ public class PlayingVideoAvtivity extends AppCompatActivity{
         });
     }
 
-    private  boolean isSupportQuality(Format format)
-    {
-        if(format.width > 1920 || format.height > 1080)
-        {
-            return  false;
-        }
-        if(format.pixelWidthHeightRatio < 1.0f || format.pixelWidthHeightRatio > 2.0f)
-        {
-            return  false;
-        }
-
-        return  true;
-    }
 
 
     @Override
@@ -401,7 +282,15 @@ public class PlayingVideoAvtivity extends AppCompatActivity{
 
     }
 
-
+    // index: 0 is mp4 track,1 is wav track
+    public void testTrack(int index)
+    {
+        MappingTrackSelector.MappedTrackInfo mappedTrackInfo = trackSelector.getCurrentMappedTrackInfo();
+        TrackGroupArray rendererTrackGroups = mappedTrackInfo.getTrackGroups(1);
+        DefaultTrackSelector.SelectionOverride selectionOverride = new DefaultTrackSelector.SelectionOverride(index, 0);
+        trackSelector.setParameters(
+                trackSelector.buildUponParameters().setSelectionOverride(1, rendererTrackGroups, selectionOverride));
+    }
 
     private  void  openSettingPlayVideo(int gravity)
     {
